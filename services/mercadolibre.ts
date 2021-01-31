d8993825c063226339c61103c832404b19aa40ab
manuhank <manuhank@gmail.com>
2021-01-31 00:54:45 -0300

    Types

--- services/mercadolibre.ts
+++ services/mercadolibre.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
 
 export const search = (query: string) => axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`);
-export const getItem = (id: string) => axios.get(`https://api.mercadolibre.com/items/${id}`)
+export const getItem = (id: string) => axios.get(`https://api.mercadolibre.com/items/${id}`);
+export const getItemDescription = (id: string) => axios.get(`https://api.mercadolibre.com/items/${id}/description`)
